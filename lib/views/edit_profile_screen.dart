ae55bfd13f37d95d7e3a36a6c8e0bfb5c8e0e2d9
Ubuntu <abrarhassan7809@gmail.com>
2025-12-25 22:00:48 +0500

    minor change in edit profile

--- lib/views/edit_profile_screen.dart
+++ lib/views/edit_profile_screen.dart
@@ -15,7 +15,15 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
     final mqWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
+      resizeToAvoidBottomInset: true,
+      extendBodyBehindAppBar: true,
+
       appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.transparent,
+        surfaceTintColor: Colors.transparent,
+        elevation: 0,
+
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
@@ -50,150 +58,152 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
       ),
 
       body: ProfileBackground(
-        child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Text(
-                    'Edit Profile',
-                    style: TextStyle(
-                      fontFamily: 'BernardMTCondensed',
-                      fontWeight: FontWeight.w400,
-                      fontSize: 24,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: mqHeight*0.1,),
-
-              Stack(
-                children: [
-                  Container(
-                    width: 110,
-                    height: 110,
-                    decoration: BoxDecoration(
-                      gradient: const LinearGradient(
-                        colors: [
-                          Color(0xFFFFE600),
-                          Color(0xFFDB2519),
-                        ],
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Edit Profile',
+                      style: TextStyle(
+                        fontFamily: 'BernardMTCondensed',
+                        fontWeight: FontWeight.w400,
+                        fontSize: 24,
+                        color: Colors.white,
                       ),
-                      borderRadius: BorderRadius.circular(100),
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(2),
+                  ],
+                ),
+                SizedBox(height: mqHeight*0.1,),
+
+                Stack(
+                  children: [
+                    Container(
+                      width: 110,
+                      height: 110,
+                      decoration: BoxDecoration(
+                        gradient: const LinearGradient(
+                          colors: [
+                            Color(0xFFFFE600),
+                            Color(0xFFDB2519),
+                          ],
+                        ),
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(2),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.black.withOpacity(0.7),
+                            borderRadius: BorderRadius.circular(98),
+                          ),
+                          child: Image.asset(
+                            'assets/images/profile_image.png',
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    Positioned(
+                      bottom: 0,
+                      right: 10,
                       child: Container(
+                        height: 30,
+                        width: 30,
                         decoration: BoxDecoration(
-                          color: Colors.black.withOpacity(0.7),
-                          borderRadius: BorderRadius.circular(98),
+                          gradient: LinearGradient(
+                            colors: [
+                              Color(0xFFFFE600),
+                              Color(0xFFDB2519),
+                            ],
+                          ),
+                          borderRadius: BorderRadius.circular(50),
                         ),
-                        child: Image.asset(
-                          'assets/images/profile_image.png',
-                        ),
+                        child: Icon(Icons.camera_alt, color: Colors.white, size: 18,),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: mqHeight*0.05,),
+
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: mqWidth * 0.07),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hint: Text(
+                        "Username",
+                        style: TextStyle(fontFamily: 'Benne', fontSize: 14),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(mqWidth * 0.5),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(mqWidth * 0.5),
+                        borderSide: const BorderSide(color: Color(0xFFDB2519)),
                       ),
                     ),
                   ),
+                ),
+                SizedBox(height: mqHeight * 0.01,),
 
-                  Positioned(
-                    bottom: 0,
-                    right: 10,
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: mqWidth * 0.07),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hint: Text(
+                        "Email Address",
+                        style: TextStyle(fontFamily: 'Benne', fontSize: 14),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(mqWidth * 0.5),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(mqWidth * 0.5),
+                        borderSide: const BorderSide(color: Color(0xFFDB2519)),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: mqHeight * 0.05,),
+
+                InkWell(
+                  onTap: () {},
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: mqWidth * 0.05),
                     child: Container(
-                      height: 30,
-                      width: 30,
+                      height: mqHeight * 0.06,
+                      width: double.infinity,
                       decoration: BoxDecoration(
                         gradient: LinearGradient(
                           colors: [
                             Color(0xFFFFE600),
                             Color(0xFFDB2519),
                           ],
+                          begin: Alignment.centerLeft,
+                          end: Alignment.centerRight,
                         ),
-                        borderRadius: BorderRadius.circular(50),
+                        borderRadius: BorderRadius.circular(40),
                       ),
-                      child: Icon(Icons.camera_alt, color: Colors.white, size: 18,),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: mqHeight*0.05,),
-
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: mqWidth * 0.07),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hint: Text(
-                      "Username",
-                      style: TextStyle(fontFamily: 'Benne', fontSize: 14),
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(mqWidth * 0.5),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(mqWidth * 0.5),
-                      borderSide: const BorderSide(color: Color(0xFFDB2519)),
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: mqHeight * 0.01,),
-
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: mqWidth * 0.07),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hint: Text(
-                      "Email Address",
-                      style: TextStyle(fontFamily: 'Benne', fontSize: 14),
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(mqWidth * 0.5),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(mqWidth * 0.5),
-                      borderSide: const BorderSide(color: Color(0xFFDB2519)),
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: mqHeight * 0.05,),
-
-              InkWell(
-                onTap: () {},
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: mqWidth * 0.05),
-                  child: Container(
-                    height: mqHeight * 0.06,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        colors: [
-                          Color(0xFFFFE600),
-                          Color(0xFFDB2519),
-                        ],
-                        begin: Alignment.centerLeft,
-                        end: Alignment.centerRight,
-                      ),
-                      borderRadius: BorderRadius.circular(40),
-                    ),
-                    child: Center(
-                      child: Text(
-                        'Update',
-                        style: TextStyle(
-                          letterSpacing: -0.3,
-                          fontFamily: 'BernardMTCondensed',
-                          fontWeight: FontWeight.w400,
-                          color: Colors.white,
-                          fontSize: 16,
+                      child: Center(
+                        child: Text(
+                          'Update',
+                          style: TextStyle(
+                            letterSpacing: -0.3,
+                            fontFamily: 'BernardMTCondensed',
+                            fontWeight: FontWeight.w400,
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
                         ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
